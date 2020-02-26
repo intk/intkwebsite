884c15a3d0673ca0047e37aa785a15447b755f01
andreesg <andreslb1@gmail.com>
2020-02-26 12:59:47 +0100

    General styles for document layout. Headings. Body text. Links. Tables. Listings

--- src/plonetheme/intkwebsite/theme/js/bundle.js
+++ src/plonetheme/intkwebsite/theme/js/bundle.js
@@ -1,17 +1,1 @@
 /* Custom JS */
-
-require([
-  'jquery',
-], function($, dep1, logger){
-  'use strict';
-  
-  // initialize only if we are in top frame
-  if (window.parent === window) {
-    
-    jQuery(document).ready(function() {
-      jQuery('body').addClass('intkwebsite-main');
-    });
-
-
-  }
-});
